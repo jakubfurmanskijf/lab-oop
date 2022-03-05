a88d5f9aa556ef5d247ef522c18b3c14150180fe
unknown <Jakub.Furmanski@microsoft.wsei.edu.pl>
2022-03-05 17:16:26 +0100

    sdaasd

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -6,7 +6,7 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wossssdrld!");
         }
     }
 }
